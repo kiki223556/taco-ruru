82090ed9fa6fae676daac9134fb4747d52e7dd77
shuaigle <layard3@gmail.com>
2022-10-25 08:03:53 +0800

    add taco order

--- src/main/java/ru/tacocloud/controller/DesignTacoController.java
+++ src/main/java/ru/tacocloud/controller/DesignTacoController.java
@@ -1,44 +1,45 @@
 package ru.tacocloud.controller;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
-import ru.tacocloud.model.Ingredient;
-import ru.tacocloud.model.Taco;
-import ru.tacocloud.model.TacoOrder;
-import ru.tacocloud.model.Type;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Slf4j  //Lombok-provided annotation. Automatically generate Logger at compilation.
-@Controller  //as controller, scanning.
-@RequestMapping("/design")  //handle requests whose path begins with /design.
-@SessionAttributes("tacoOrder")  //put into model and maintain
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import lombok.extern.slf4j.Slf4j;
+import ru.tacocloud.model.Ingredient;
+import ru.tacocloud.model.IngredientType;
+import ru.tacocloud.model.Taco;
+import ru.tacocloud.model.TacoOrder;
+
+@Slf4j // Lombok-provided annotation. Automatically generate Logger at compilation.
+@Controller // as controller, scanning.
+@RequestMapping("/design") // handle requests whose path begins with /design.
+@SessionAttributes("tacoOrder") // put into model and maintain
 public class DesignTacoController {
 
     @ModelAttribute
     public void addIngredientsToModel(Model model) {
-        List<Ingredient> ingredients = Arrays.asList (
-                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-                new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-                new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-                new Ingredient("CHED", "Cheddar", Type.CHEESE),
-                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-                new Ingredient("SLSA", "Salsa", Type.SAUCE),
-                new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-        );
+        List<Ingredient> ingredients = Arrays.asList(
+                new Ingredient("FLTO", "Flour Tortilla", IngredientType.WRAP),
+                new Ingredient("COTO", "Corn Tortilla", IngredientType.WRAP),
+                new Ingredient("GRBF", "Ground Beef", IngredientType.PROTEIN),
+                new Ingredient("CARN", "Carnitas", IngredientType.PROTEIN),
+                new Ingredient("TMTO", "Diced Tomatoes", IngredientType.VEGGIES),
+                new Ingredient("LETC", "Lettuce", IngredientType.VEGGIES),
+                new Ingredient("CHED", "Cheddar", IngredientType.CHEESE),
+                new Ingredient("JACK", "Monterrey Jack", IngredientType.CHEESE),
+                new Ingredient("SLSA", "Salsa", IngredientType.SAUCE),
+                new Ingredient("SRCR", "Sour Cream", IngredientType.SAUCE));
 
-        Type[] types = Type.values();
-        for (Type type : types) {
+        IngredientType[] types = IngredientType.values();
+        for (IngredientType type : types) {
             model.addAttribute(type.toString().toLowerCase(),
                     filterByType(ingredients, type));
         }
@@ -54,13 +55,25 @@ public class DesignTacoController {
         return new Taco();
     }
 
+    // 導向至/design時自動觸發
+    // design為視圖view得名稱
     @GetMapping
     public String showDesignForm() {
         return "design";
     }
 
+    // 目前沒做啥事，重新導向redirect至其他頁面
+    // 用來處理/design template內的form表單所填寫的內容
+    @PostMapping
+    public String processTaco(Taco taco) {
+        // Save the taco...
+        // We'll do this in chapter 3
+        log.info("Processing taco: " + taco); // 日誌print
+        return "redirect:/orders/current";
+    }
+
     private Iterable<Ingredient> filterByType(
-            List<Ingredient> ingredients, Type type) {
+            List<Ingredient> ingredients, IngredientType type) {
         return ingredients
                 .stream()
                 .filter(x -> x.getType().equals(type))
---
+++ src/main/java/ru/tacocloud/controller/OrderController.java
@@ -0,0 +1,31 @@
+package ru.tacocloud.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+import ru.tacocloud.model.TacoOrder;
+
+@Slf4j // 記錄日誌 -> 編譯時處理
+@Controller // 控制器處理前端傳來的請求
+@RequestMapping("/orders") // 自己的門牌位置，處理前端要求
+public class OrderController {
+
+    // 前端使用get方法，後端對應接收位置/orders/current
+    @GetMapping("/current")
+    public String orderForm(Model model) {
+        model.addAttribute("tacoOrder", new TacoOrder());
+        return "orderForm";
+    }
+
+    // 因為在orderForm template的表單內把action設為/orders地址
+    // 這裡要有method能夠接收
+    @PostMapping
+    public String processOrder(TacoOrder order) {
+        log.info("Order submitted: " + order); // print log
+        return "redirect:/"; // 重新導向至根目錄
+    }
+}
