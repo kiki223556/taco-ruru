05038981819b8f219194d60032f1b49678b79b00
shuaigle <layard3@gmail.com>
2022-11-07 21:59:37 +0800

    add hibernate example

--- src/main/java/ru/tacocloud/repository/OrderRepository.java
+++ src/main/java/ru/tacocloud/repository/OrderRepository.java
@@ -1,9 +1,19 @@
 package ru.tacocloud.repository;
 
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import ru.tacocloud.model.TacoOrder;
 
-public interface OrderRepository
-                extends CrudRepository<TacoOrder, Long> {
+public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
+    List<TacoOrder> findByDeliveryZip(String deliveryZip);
+
+    List<TacoOrder> readOrdersByDeliveryZipAndPlacedAtBetween(
+            String deliveryZip, Date startDate, Date endDate);
+
+    @Query("Order o where o.deliveryCity='Seattle'")
+    List<TacoOrder> readOrdersDeliveredInSeattle();
 }
