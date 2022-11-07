cc0765e363328031dd7f1582c4a4e19b2a1218cf
shuaigle <layard3@gmail.com>
2022-11-07 08:38:01 +0800

    persist data  with repoistory bean

--- src/main/java/ru/tacocloud/TacoCloudApplication.java
+++ src/main/java/ru/tacocloud/TacoCloudApplication.java
@@ -1,7 +1,13 @@
 package ru.tacocloud;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import ru.tacocloud.model.Ingredient;
+import ru.tacocloud.model.IngredientType;
+import ru.tacocloud.repository.IngredientRepository;
 
 /*
 @SpringBootApplication為executable JAR運行時的切入點，包含以下3個註解
@@ -14,6 +20,26 @@ public class TacoCloudApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TacoCloudApplication.class, args);
+    }
+
+    // they are using the repositories to create the persisted objects instead of an
+    // SQL script
+    // they’ll work equally well for relational databases or non-relational
+    // databases
+    @Bean
+    public ApplicationRunner dataLoader(IngredientRepository repo) {
+        return args -> {
+            repo.save(new Ingredient("FLTO", "Flour Tortilla", IngredientType.WRAP));
+            repo.save(new Ingredient("COTO", "Corn Tortilla", IngredientType.WRAP));
+            repo.save(new Ingredient("GRBF", "Ground Beef", IngredientType.PROTEIN));
+            repo.save(new Ingredient("CARN", "Carnitas", IngredientType.PROTEIN));
+            repo.save(new Ingredient("TMTO", "Diced Tomatoes", IngredientType.VEGGIES));
+            repo.save(new Ingredient("LETC", "Lettuce", IngredientType.VEGGIES));
+            repo.save(new Ingredient("CHED", "Cheddar", IngredientType.CHEESE));
+            repo.save(new Ingredient("JACK", "Monterrey Jack", IngredientType.CHEESE));
+            repo.save(new Ingredient("SLSA", "Salsa", IngredientType.SAUCE));
+            repo.save(new Ingredient("SRCR", "Sour Cream", IngredientType.SAUCE));
+        };
     }
 
 }
