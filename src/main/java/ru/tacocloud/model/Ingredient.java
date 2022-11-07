dda3dc029074c960cb4a4c23d8948a13eb81bd00
shuaigle <layard3@gmail.com>
2022-11-07 09:23:36 +0800

    annotating hibernate model

--- src/main/java/ru/tacocloud/model/Ingredient.java
+++ src/main/java/ru/tacocloud/model/Ingredient.java
@@ -6,12 +6,14 @@ import org.springframework.data.annotation.Id;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-//lombok：語法糖，在編譯時自動生成執行所需JavaBean methods，包含@Getter, @Setter, @ToString等。
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-//@Data：生成一個建構子，自動生成@Getter, @Setter, @ToString, @EqualsAndHashCode, @RequiredArgsConstructor。
-//使其可接收下列final的特性，可消除冗長的code和提高開發效率。
+// @Data：生成一個建構子，自動生成@Getter, @Setter, @ToString, @EqualsAndHashCode, @RequiredArgsConstructor。
+// 使其可接收下列final的特性，可消除冗長的code和提高開發效率。
+// JPA requires that entities have a no-arguments constructor
+// final attribute setting -> force = true
+// settign default value of null, 0, or false
 @Data
 @Entity
 @AllArgsConstructor
--- src/main/java/ru/tacocloud/model/Taco.java
+++ src/main/java/ru/tacocloud/model/Taco.java
@@ -4,6 +4,9 @@ import java.util.Date;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -16,7 +19,9 @@ import lombok.Data;
 public class Taco {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private Date createdAt = new Date();
 
     // 新增表單限制
@@ -25,7 +30,11 @@ public class Taco {
     @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name; // 只回傳taco名稱，所以用string
 
-    @NotNull
     @Size(min = 1, message = "You must choose at least 1 ingredient")
-    private List<IngredientRef> ingredients; // 可能是0到多選，所以用list
+    @ManyToMany()
+    private List<Ingredient> ingredients; // 可能是0到多選，所以用list
+
+    public void addIngredient(Ingredient ingredient) {
+        this.ingredients.add(ingredient);
+    }
 }
