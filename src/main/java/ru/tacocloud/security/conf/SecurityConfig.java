cd19977ad428a197591f563d61534dc75ba32efa
shuaigle <layard3@gmail.com>
2022-11-14 08:07:22 +0800

    add conf

--- src/main/java/ru/tacocloud/security/conf/SecurityConfig.java
+++ src/main/java/ru/tacocloud/security/conf/SecurityConfig.java
@@ -2,14 +2,28 @@ package ru.tacocloud.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import ru.tacocloud.model.auth.User;
+import ru.tacocloud.repository.UserRepository;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepo) {
+        return username -> {
+            User user = userRepo.findByUsername(username);
+            if (user != null)
+                return user;
+            throw new UsernameNotFoundException("User '" + username + "' not found");
+        };
     }
 
     // @Bean
